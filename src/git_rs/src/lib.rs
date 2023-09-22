fbc2909022a719072ae92e0e9c1a437056c800da
shenjack <3695888@qq.com>
2023-09-23 03:16:54 +0800

    add odb

--- src/git_rs/src/lib.rs
+++ src/git_rs/src/lib.rs
@@ -1,5 +1,10 @@
 mod changes;
 
-fn main() {
-    println!("Hello, world!");
+use pyo3::prelude::*;
+
+#[test]
+fn diff() {
+    use gix_diff::tree::{Recorder, Changes};
+    // 从仓库中读取两个树对象
+    let mut record = Recorder::track_location();
 }
