2df1ba76bdf01ca2995eb0e343cc0dbe399493df
shenjack <3695888@qq.com>
2023-09-23 12:36:56 +0800

    我在整一种不怎么新的东西

--- zip-test/src/main.rs
+++ zip-test/src/main.rs
@@ -1,5 +1,40 @@
 use std::fs;
 use std::io::{Seek, Write};
+use blake3::Hasher;
+use base16384::Base16384Utf8;
+
+pub struct FileMetaData {
+    pub file: Vec<u8>,
+    pub file_size: u64,
+    pub file_blake3: Hasher,
+}
+
+impl FileMetaData {
+    pub fn as_string(&self) -> String {
+        let mut blake_data = self.file_blake3.finalize_xof();
+        let mut buff = [0; 512];
+        blake_data.fill(&mut buff);
+        let blake3_str = Base16384Utf8::encode(&buff);
+        format!("file:{}|{}|by-shenjack", self.file_size, blake3_str)
+    }
+
+    pub fn new(file: Vec<u8>) -> Self {
+        let mut file_blake3 = Hasher::new();
+        file_blake3.update(&file);
+        let file_size = file.len() as u64;
+        Self {
+            file,
+            file_size,
+            file_blake3,
+        }
+    }
+
+    /// 从给定数据中解析出 文件+文件大小+文件blake3
+    pub fn from_data(data: &[u8]) -> Option<Self> {
+
+        None
+    }
+}
 
 fn main() {
     // 获取当前可执行文件的路径
