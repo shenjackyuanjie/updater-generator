95f5242d9bc5e17330d3967fd40092f10ee4709d
shenjack <3695888@qq.com>
2023-09-23 12:48:34 +0800

    还是用回String

--- zip-test/src/main.rs
+++ zip-test/src/main.rs
@@ -6,33 +6,51 @@ use base16384::Base16384Utf8;
 pub struct FileMetaData {
     pub file: Vec<u8>,
     pub file_size: u64,
-    pub file_blake3: Hasher,
+    pub file_blake3: String,
 }
 
 impl FileMetaData {
     pub fn as_string(&self) -> String {
-        let mut blake_data = self.file_blake3.finalize_xof();
-        let mut buff = [0; 512];
-        blake_data.fill(&mut buff);
-        let blake3_str = Base16384Utf8::encode(&buff);
-        format!("file:{}|{}|by-shenjack", self.file_size, blake3_str)
+        format!("|file:{}|{}|by-shenjack", self.file_size, self.file_blake3)
     }
 
     pub fn new(file: Vec<u8>) -> Self {
+        let file_size = file.len() as u64;
+
         let mut file_blake3 = Hasher::new();
         file_blake3.update(&file);
-        let file_size = file.len() as u64;
+        let mut blake_data = file_blake3.finalize_xof();
+
+        let mut buff = [0; 512];
+        blake_data.fill(&mut buff);
+
+        let blake3_str = Base16384Utf8::encode(&buff);
         Self {
             file,
             file_size,
-            file_blake3,
+            file_blake3: blake3_str,
         }
     }
 
     /// 从给定数据中解析出 文件+文件大小+文件blake3
     pub fn from_data(data: &[u8]) -> Option<Self> {
-
-        None
+        let data = String::from_utf8_lossy(data);
+        if !data.ends_with("|by-shenjack") {
+            return None;
+        }
+        let data = data.trim_end_matches("|by-shenjack");
+        let data: Vec<&str> = data.split('|').collect();
+        // xxxxxxxxx|file:xxxxxx|xxxxxx
+        if data.len() < 3 {
+            return None;
+        }
+        let file_size = data[1].trim_start_matches("file:").parse::<u64>().unwrap();
+        let file_blake3 = data[2].to_string();
+        Some(Self {
+            file: Vec::new(),
+            file_size,
+            file_blake3,
+        })
     }
 }
 
