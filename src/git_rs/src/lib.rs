990cde429f54d8df8e2b95c6589f33b52eedb21b
shenjack <3695888@qq.com>
2023-09-23 09:34:34 +0800

    怪折磨人的

--- src/git_rs/src/lib.rs
+++ src/git_rs/src/lib.rs
@@ -1,6 +1,6 @@
-use gix_hash::ObjectId;
-use gix_odb::Find;
-use gix_ref::bstr::ByteSlice;
+use gix_hash::{oid, ObjectId};
+use gix_object::{bstr::ByteSlice, tree::EntryMode, TreeRefIter};
+use gix_odb::pack::Find;
 use std::collections::HashMap;
 
 mod changes;
@@ -69,15 +69,20 @@ fn diff() {
     // 从仓库中读取两个树对象
     let mut buffer = Vec::new();
     let mut repo = gix_odb::at("../../tests/try_diff/.git/objects").unwrap();
-    let tree1 = repo
+    let tree_id = repo
         .try_find(
             &hex_to_id("9d394f2fd6abb585e4126f9995ca2e187d164900"),
             &mut buffer,
         )
         .unwrap()
         .unwrap()
-        .try_into_tree_iter()
-        .unwrap();
+        .0
+        .decode()
+        .unwrap()
+        .into_commit()
+        .unwrap()
+        .tree();
+    let tree = repo.find_tree(&tree_id, &mut buffer).unwrap().0;
 
     // let mut record = Recorder::track_location();
 }
